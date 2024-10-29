66314cedbe3bd176f2e41a3839dd5facbd7e7618
Ricardo <rricardo.23.11.2022@gmail.com>
2024-10-29 08:39:24 -0300

    fix router

--- src/Routers/PublicRoutes/index.tsx
+++ src/Routers/PublicRoutes/index.tsx
@@ -1,11 +1,7 @@
 import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "../../Store";
 
 export const PublicRoute = ({ children }: { children: JSX.Element }) => {
-  const isAuthenticated = useSelector(
-    (state: RootState) => state.auth.isAuthenticated
-  );
+  const isAuthenticated = localStorage.getItem("authToken") !== null;
 
   return isAuthenticated ? <Navigate to="/Dashboard" /> : children;
 };
