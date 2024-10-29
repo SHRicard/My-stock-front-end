086ef90431bc266f79ee6ce95881281c1a2d2f04
Ricardo <rricardo.23.11.2022@gmail.com>
2024-10-29 10:12:15 -0300

    local store fix

--- src/Utils/Token/index.ts
+++ src/Utils/Token/index.ts
@@ -1,22 +1,18 @@
 import { jwtDecode } from "jwt-decode";
 
-// Función para verificar si el token JWT es válido
 export const isTokenValid = (): boolean => {
-  const token =
-    localStorage.getItem("authToken") || sessionStorage.getItem("authToken");
-  if (!token) return false; // No hay token almacenado
+  const token = sessionStorage.getItem("authToken");
+  if (!token) return false;
 
   try {
-    const decodedToken: any = jwtDecode(token); // Decodificamos el token
-    const currentTime = Date.now() / 1000; // Tiempo actual en segundos
+    const decodedToken: any = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
 
     if (decodedToken.exp < currentTime) {
-      // Si el token ha expirado, lo eliminamos
-      localStorage.removeItem("authToken");
       sessionStorage.removeItem("authToken");
-      return false; // El token ha expirado
+      return false;
     }
-    return true; // El token es válido
+    return true;
   } catch (error) {
     console.error("Token inválido:", error);
     return false;
